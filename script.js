7f9db2716b588a01a21a5e3c9a7f988d74b92a70
Aakash Kumar Indev <aakashkumar8378@gmail.com>
2026-02-23 11:30:31 +0530

    I am Aakash

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 alert("Hello! Welcome to my resume website. Feel free to explore and learn more about my skills and experience. If you have any questions, don't hesitate to reach out. Enjoy your visit!");
-console.log("Hello again");
+console.log("Hello Aakash");
